33b6fc9180a38d4f91ceff0c1f79202c0149e2e8
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2025-05-13 08:30:21 -0700

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
+// x @ts-check - disabling for now
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  }
+});
